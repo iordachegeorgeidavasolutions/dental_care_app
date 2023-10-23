cc14a3f32ebd7becc8c880911f62b9512a2b2d8e
IT2 iDava <it2.idavasolutions@outlook.com>
2023-10-23 13:26:50 +0300

    Removed profile pic

--- lib/widgets/items/image_widget.dart
+++ lib/widgets/items/image_widget.dart
@@ -1,120 +1,120 @@
-import 'dart:io';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
+// import 'dart:io';
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+// import 'package:image_picker/image_picker.dart';
 
-class ImageWidget extends StatelessWidget {
-  final File image;
-  final ValueChanged<ImageSource> onClicked;
+// class ImageWidget extends StatelessWidget {
+//   final File image;
+//   final ValueChanged<ImageSource> onClicked;
 
-  const ImageWidget({
-    Key? key,
-    required this.image,
-    required this.onClicked,
-  }) : super(key: key);
+//   const ImageWidget({
+//     Key? key,
+//     required this.image,
+//     required this.onClicked,
+//   }) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    final color = Theme.of(context).colorScheme.primary;
+//   @override
+//   Widget build(BuildContext context) {
+//     final color = Theme.of(context).colorScheme.primary;
 
-    return Center(
-      child: Stack(
-        children: [
-          buildImage(context),
-          Positioned(
-            bottom: 0,
-            right: 4,
-            child: buildEditIcon(color),
-          ),
-        ],
-      ),
-    );
-  }
+//     return Center(
+//       child: Stack(
+//         children: [
+//           buildImage(context),
+//           Positioned(
+//             bottom: 0,
+//             right: 4,
+//             child: buildEditIcon(color),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
 
-  Widget buildImage(BuildContext context) {
-    final imagePath = this.image.path;
-    final image = imagePath.contains('https://') ? NetworkImage(imagePath) : FileImage(File(imagePath));
+//   Widget buildImage(BuildContext context) {
+//     final imagePath = this.image.path;
+//     final image = imagePath.contains('https://') ? NetworkImage(imagePath) : FileImage(File(imagePath));
 
-    return ClipOval(
-      child: Material(
-        color: Colors.transparent,
-        child: Ink.image(
-          image: image as ImageProvider,
-          fit: BoxFit.cover,
-          width: 160,
-          height: 160,
-          child: InkWell(
-            onTap: () async {
-              final source = await showImageSource(context);
-              if (source == null) return;
+//     return ClipOval(
+//       child: Material(
+//         color: Colors.transparent,
+//         child: Ink.image(
+//           image: image as ImageProvider,
+//           fit: BoxFit.cover,
+//           width: 160,
+//           height: 160,
+//           child: InkWell(
+//             onTap: () async {
+//               final source = await showImageSource(context);
+//               if (source == null) return;
 
-              onClicked(source);
-            },
-          ),
-        ),
-      ),
-    );
-  }
+//               onClicked(source);
+//             },
+//           ),
+//         ),
+//       ),
+//     );
+//   }
 
-  Widget buildEditIcon(Color color) => buildCircle(
-        color: Colors.white,
-        all: 3,
-        child: buildCircle(
-          color: color,
-          all: 10,
-          child: Icon(Icons.add_a_photo, color: Colors.white, size: 28),
-        ),
-      );
+//   Widget buildEditIcon(Color color) => buildCircle(
+//         color: Colors.white,
+//         all: 3,
+//         child: buildCircle(
+//           color: color,
+//           all: 10,
+//           child: Icon(Icons.add_a_photo, color: Colors.white, size: 28),
+//         ),
+//       );
 
-  Widget buildCircle({
-    required Widget child,
-    required double all,
-    required Color color,
-  }) =>
-      ClipOval(
-        child: Container(
-          padding: EdgeInsets.all(all),
-          color: color,
-          child: child,
-        ),
-      );
+//   Widget buildCircle({
+//     required Widget child,
+//     required double all,
+//     required Color color,
+//   }) =>
+//       ClipOval(
+//         child: Container(
+//           padding: EdgeInsets.all(all),
+//           color: color,
+//           child: child,
+//         ),
+//       );
 
-  Future<ImageSource?> showImageSource(BuildContext context) async {
-    if (Platform.isIOS) {
-      return showCupertinoModalPopup<ImageSource>(
-        context: context,
-        builder: (context) => CupertinoActionSheet(
-          actions: [
-            CupertinoActionSheetAction(
-              child: Text('Camera'),
-              onPressed: () => Navigator.of(context).pop(ImageSource.camera),
-            ),
-            CupertinoActionSheetAction(
-              child: Text('Gallery'),
-              onPressed: () => Navigator.of(context).pop(ImageSource.gallery),
-            ),
-          ],
-        ),
-      );
-    } else {
-      return showModalBottomSheet(
-        context: context,
-        builder: (context) => Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            ListTile(
-              leading: Icon(Icons.camera_alt),
-              title: Text('Camera'),
-              onTap: () => Navigator.of(context).pop(ImageSource.camera),
-            ),
-            ListTile(
-              leading: Icon(Icons.image),
-              title: Text('Gallery'),
-              onTap: () => Navigator.of(context).pop(ImageSource.gallery),
-            )
-          ],
-        ),
-      );
-    }
-  }
-}
+//   Future<ImageSource?> showImageSource(BuildContext context) async {
+//     if (Platform.isIOS) {
+//       return showCupertinoModalPopup<ImageSource>(
+//         context: context,
+//         builder: (context) => CupertinoActionSheet(
+//           actions: [
+//             CupertinoActionSheetAction(
+//               child: Text('Camera'),
+//               onPressed: () => Navigator.of(context).pop(ImageSource.camera),
+//             ),
+//             CupertinoActionSheetAction(
+//               child: Text('Gallery'),
+//               onPressed: () => Navigator.of(context).pop(ImageSource.gallery),
+//             ),
+//           ],
+//         ),
+//       );
+//     } else {
+//       return showModalBottomSheet(
+//         context: context,
+//         builder: (context) => Column(
+//           mainAxisSize: MainAxisSize.min,
+//           children: [
+//             ListTile(
+//               leading: Icon(Icons.camera_alt),
+//               title: Text('Camera'),
+//               onTap: () => Navigator.of(context).pop(ImageSource.camera),
+//             ),
+//             ListTile(
+//               leading: Icon(Icons.image),
+//               title: Text('Gallery'),
+//               onTap: () => Navigator.of(context).pop(ImageSource.gallery),
+//             )
+//           ],
+//         ),
+//       );
+//     }
+//   }
+// }
--- lib/widgets/items/profilepiclogic.dart
+++ lib/widgets/items/profilepiclogic.dart
@@ -1,126 +1,126 @@
-import 'dart:io';
+// import 'dart:io';
 
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:path/path.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
+// import 'package:image_picker/image_picker.dart';
+// import 'package:path_provider/path_provider.dart';
+// import 'package:path/path.dart';
 
-import 'image_widget.dart';
+// import 'image_widget.dart';
 
-Future main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-    DeviceOrientation.portraitDown,
-  ]);
+// Future main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await SystemChrome.setPreferredOrientations([
+//     DeviceOrientation.portraitUp,
+//     DeviceOrientation.portraitDown,
+//   ]);
 
-  runApp(MyApp());
-}
+//   runApp(MyApp());
+// }
 
-class MyApp extends StatelessWidget {
-  static final String title = 'Image Picker';
+// class MyApp extends StatelessWidget {
+//   static final String title = 'Image Picker';
 
-  @override
-  Widget build(BuildContext context) => MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: title,
-        theme: ThemeData(primarySwatch: Colors.blue),
-        home: MainPage(),
-      );
-}
+//   @override
+//   Widget build(BuildContext context) => MaterialApp(
+//         debugShowCheckedModeBanner: false,
+//         title: title,
+//         theme: ThemeData(primarySwatch: Colors.blue),
+//         home: MainPage(),
+//       );
+// }
 
-class MainPage extends StatefulWidget {
-  @override
-  _MainPageState createState() => _MainPageState();
-}
+// class MainPage extends StatefulWidget {
+//   @override
+//   _MainPageState createState() => _MainPageState();
+// }
 
-class _MainPageState extends State<MainPage> {
-  File? image;
+// class _MainPageState extends State<MainPage> {
+//   File? image;
 
-  Future pickImage(ImageSource source) async {
-    try {
-      final image = await ImagePicker().pickImage(source: source);
-      if (image == null) return;
+//   Future pickImage(ImageSource source) async {
+//     try {
+//       final image = await ImagePicker().pickImage(source: source);
+//       if (image == null) return;
 
-      /// Alternatively save picked image permanently
-      //final imagePermanent = await saveImagePermanently(image.path);
-      //setState(() => this.image = imagePermanent);
+//       /// Alternatively save picked image permanently
+//       //final imagePermanent = await saveImagePermanently(image.path);
+//       //setState(() => this.image = imagePermanent);
 
-      final imageTemporary = File(image.path);
-      setState(() => this.image = imageTemporary);
-    } on PlatformException catch (e) {
-      print('Failed to pick image: $e');
-    }
-  }
+//       final imageTemporary = File(image.path);
+//       setState(() => this.image = imageTemporary);
+//     } on PlatformException catch (e) {
+//       print('Failed to pick image: $e');
+//     }
+//   }
 
-  Future<File> saveImagePermanently(String imagePath) async {
-    final directory = await getApplicationDocumentsDirectory();
-    final name = basename(imagePath);
-    final image = File('${directory.path}/$name');
+//   Future<File> saveImagePermanently(String imagePath) async {
+//     final directory = await getApplicationDocumentsDirectory();
+//     final name = basename(imagePath);
+//     final image = File('${directory.path}/$name');
 
-    return File(imagePath).copy(image.path);
-  }
+//     return File(imagePath).copy(image.path);
+//   }
 
-  @override
-  Widget build(BuildContext context) => Scaffold(
-        backgroundColor: Colors.amber.shade300,
-        body: Container(
-          padding: EdgeInsets.all(32),
-          child: Column(
-            children: [
-              Spacer(),
-              image != null
-                  ? ImageWidget(
-                      image: image!,
-                      onClicked: (source) => pickImage(source),
-                    )
-                  : FlutterLogo(size: 160),
-              const SizedBox(height: 24),
-              Text(
-                'Image Picker',
-                style: TextStyle(
-                  fontSize: 48,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 48),
-              buildButton(
-                title: 'Pick Gallery',
-                icon: Icons.image_outlined,
-                onClicked: () => pickImage(ImageSource.gallery),
-              ),
-              const SizedBox(height: 24),
-              buildButton(
-                title: 'Pick Camera',
-                icon: Icons.camera_alt_outlined,
-                onClicked: () => pickImage(ImageSource.camera),
-              ),
-              Spacer(),
-            ],
-          ),
-        ),
-      );
+//   @override
+//   Widget build(BuildContext context) => Scaffold(
+//         backgroundColor: Colors.amber.shade300,
+//         body: Container(
+//           padding: EdgeInsets.all(32),
+//           child: Column(
+//             children: [
+//               Spacer(),
+//               image != null
+//                   ? ImageWidget(
+//                       image: image!,
+//                       onClicked: (source) => pickImage(source),
+//                     )
+//                   : FlutterLogo(size: 160),
+//               const SizedBox(height: 24),
+//               Text(
+//                 'Image Picker',
+//                 style: TextStyle(
+//                   fontSize: 48,
+//                   fontWeight: FontWeight.bold,
+//                 ),
+//               ),
+//               const SizedBox(height: 48),
+//               buildButton(
+//                 title: 'Pick Gallery',
+//                 icon: Icons.image_outlined,
+//                 onClicked: () => pickImage(ImageSource.gallery),
+//               ),
+//               const SizedBox(height: 24),
+//               buildButton(
+//                 title: 'Pick Camera',
+//                 icon: Icons.camera_alt_outlined,
+//                 onClicked: () => pickImage(ImageSource.camera),
+//               ),
+//               Spacer(),
+//             ],
+//           ),
+//         ),
+//       );
 
-  Widget buildButton({
-    required String title,
-    required IconData icon,
-    required VoidCallback onClicked,
-  }) =>
-      ElevatedButton(
-        style: ElevatedButton.styleFrom(
-          minimumSize: Size.fromHeight(56),
-          primary: Colors.white,
-          onPrimary: Colors.black,
-          textStyle: TextStyle(fontSize: 20),
-        ),
-        child: Row(
-          children: [
-            Icon(icon, size: 28),
-            const SizedBox(width: 16),
-            Text(title),
-          ],
-        ),
-        onPressed: onClicked,
-      );
-}
+//   Widget buildButton({
+//     required String title,
+//     required IconData icon,
+//     required VoidCallback onClicked,
+//   }) =>
+//       ElevatedButton(
+//         style: ElevatedButton.styleFrom(
+//           minimumSize: Size.fromHeight(56),
+//           primary: Colors.white,
+//           onPrimary: Colors.black,
+//           textStyle: TextStyle(fontSize: 20),
+//         ),
+//         child: Row(
+//           children: [
+//             Icon(icon, size: 28),
+//             const SizedBox(width: 16),
+//             Text(title),
+//           ],
+//         ),
+//         onPressed: onClicked,
+//       );
+// }
