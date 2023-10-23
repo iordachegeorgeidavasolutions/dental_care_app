07bba946c651fc5b52d3d07fa96d5fa7dfb1a946
IT2 iDava <it2.idavasolutions@outlook.com>
2023-10-23 13:19:48 +0300

    Updated data for client's Locations

--- lib/data/locatii_data.dart
+++ lib/data/locatii_data.dart
@@ -2,27 +2,27 @@ import './locatii_model.dart';
 
 List<Locatie> locatiiList = [
   Locatie(
-    nume: "DentoCare Coltea",
-    adresa: "Str. Strada Mavrogheni Nicolae 4, sector 3, Bucuresti",
-    maps: "Strada Mavrogheni Nicolae 4, Bucuresti",
+    nume: "DentoCare Colțea",
+    adresa: "Str. Strada Mavrogheni Nicolae 4, sector 3, București",
+    maps: "Strada Mavrogheni Nicolae 4, București",
     telefon: "0758 067 270",
   ),
   Locatie(
     nume: "DentoCare Minulescu",
-    adresa: "Strada Ion Minulescu 45, sector 3, Bucuresti",
-    maps: "Str. Ion Minulescu nr. 45, Bucuresti",
+    adresa: "Strada Ion Minulescu 45, sector 3, București",
+    maps: "Str. Ion Minulescu nr. 45, București",
     telefon: "0720 134 665",
   ),
   Locatie(
     nume: "DentoCare Virtutii",
     adresa: "Soseaua Virtutii 46A, sector 4, București",
-    maps: "Sos. Virtutii nr. 46A, Bucuresti",
+    maps: "Sos. Virtutii nr. 46A, București",
     telefon: "0770 152 117",
   ),
   Locatie(
     nume: "DentoCare Lujerului",
     adresa: "Sos. Virtutii 2C, sector 4, București",
-    maps: "Sos. Virtutii nr. 2C, Bucuresti",
+    maps: "Sos. Virtutii nr. 2C, București",
     telefon: "0758 052 040",
   ),
 ];
